ac44177621f4036a8c97e9ae54be44e9218adfa2
ilisau <ilisov.laba@solvd.com>
2023-01-05 12:57:34 +0300

    feat: Implement migration

---
+++ src/main/resources/data.sql
@@ -0,0 +1,54 @@
+insert into couriers (first_name, last_name, phone_number, created_at, last_active_at, status)
+values ('John', 'Doe', '1234567890', '2023-01-01 00:00:00', '2023-01-01 00:00:00', 'AVAILABLE'),
+       ('Michael', 'Depp', '12345', '2022-12-01 00:00:00', '2022-12-05 00:00:00', 'UNAVAILABLE'),
+       ('Bob', 'Jonson', '123456', '2022-11-05 00:00:00', '2022-12-03 00:00:00', 'ON_DELIVERY');
+
+insert into restaurants (name, description)
+values ('McDonalds', 'Вкусно и точка');
+
+insert into addresses (street_name, house_number, floor_number, flat_number)
+values ('Lenina', '1', '1', '1'),
+       ('Lenina', '2', '2', '2'),
+       ('Lenina', '3', '3', '3');
+
+insert into restaurants_addresses (restaurant_id, address_id)
+values (1, 1);
+
+insert into employees (name, position, restaurant_id)
+values ('John', 'Cook', 1),
+       ('Michael', 'Cook', 1),
+       ('Bob', 'Manager', 1);
+
+insert into restaurants_employees (restaurant_id, employee_id)
+values (1, 1),
+       (1, 2),
+       (1, 3);
+
+insert into carts (user_id)
+values (1);
+
+insert into users (name, email, password, phone_number, cart_id, created_at)
+values ('Peter', 'peter@gmail.com', '$2a$12$Iq9jB0.zAt/VA.XSeWrHb.jpeWUAP6AXqP4oH3pK8hhOOfPwMzbA2', '1234', 1, '2022-12-01 00:00:00');
+
+insert into users_addresses (user_id, address_id)
+values (1, 2);
+
+insert into items (name, description, price, type, available, restaurant_id)
+values ('Burger', 'Burger', 100, 'FOOD', true, 1),
+       ('Fries', 'Fries', 50, 'FOOD', true, 1),
+       ('Coca-Cola', 'Coca-Cola', 50, 'DRINK', true, 1);
+
+insert into restaurants_items (restaurant_id, item_id)
+values (1, 1),
+       (1, 2),
+       (1, 3);
+
+insert into carts_items (cart_id, item_id)
+values (1, 1),
+       (1, 2);
+
+insert into orders (user_id, address_id, restaurant_id, cart_id, status, courier_id, created_at, delivered_at)
+values (1, 2, 1, 1, 'DELIVERED', 1, '2022-12-01 00:00:00', '2022-12-01 00:00:00');
+
+insert into users_orders (user_id, order_id)
+values (1, 1);
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,140 @@
+create table if not exists couriers
+(
+    id             bigserial      not null,
+    first_name     varchar(45) not null,
+    last_name      varchar(45) not null,
+    phone_number   varchar(45) not null unique,
+    created_at     timestamp   not null,
+    last_active_at timestamp   not null,
+    status         varchar(45) not null,
+    primary key (id)
+
+);
+
+create table if not exists restaurants
+(
+    id          bigserial      not null,
+    name        varchar(45) not null unique,
+    description varchar(255),
+    primary key (id)
+);
+
+create table if not exists employees
+(
+    id            bigserial      not null,
+    name          varchar(45) not null,
+    position      varchar(45) not null,
+    restaurant_id bigserial      not null,
+    primary key (id),
+    constraint FK_employees_restaurants foreign key (restaurant_id) references restaurants (id) on delete cascade
+);
+
+create table if not exists restaurants_employees
+(
+    restaurant_id bigserial not null,
+    employee_id   bigserial not null,
+    constraint FK_restaurants_employees_restaurants foreign key (restaurant_id) references restaurants (id) on delete cascade,
+    constraint FK_restaurants_employees_employees foreign key (employee_id) references employees (id) on delete cascade
+);
+
+create table if not exists addresses
+(
+    id           bigserial      not null,
+    street_name  varchar(45) not null,
+    house_number int         not null,
+    floor_number int,
+    flat_number  int,
+    primary key (id)
+);
+
+create table if not exists restaurants_addresses
+(
+    restaurant_id bigserial not null,
+    address_id    bigserial not null,
+    constraint FK_restaurants_addresses_restaurants foreign key (restaurant_id) references restaurants (id) on delete cascade,
+    constraint FK_restaurants_addresses_addresses foreign key (address_id) references addresses (id) on delete cascade
+);
+
+create table if not exists items
+(
+    id            bigserial           not null,
+    name          varchar(45)      not null,
+    description   varchar(255),
+    price         double precision not null,
+    type          varchar(45)      not null,
+    available     boolean          not null,
+    restaurant_id bigserial           not null,
+    primary key (id),
+    constraint FK_items_restaurants foreign key (restaurant_id) references restaurants (id) on delete cascade
+);
+
+create table if not exists restaurants_items
+(
+    restaurant_id bigserial not null,
+    item_id       bigserial not null,
+    constraint FK_restaurants_items_items foreign key (item_id) references items (id) on delete cascade,
+    constraint FK_restaurants_items_restaurants foreign key (restaurant_id) references restaurants (id) on delete cascade
+);
+
+create table if not exists carts
+(
+    id      bigserial not null,
+    user_id bigserial not null,
+    primary key (id)
+);
+
+create table if not exists carts_items
+(
+    cart_id bigserial not null,
+    item_id bigserial not null,
+    constraint FK_carts_items_carts foreign key (cart_id) references carts (id) on delete cascade,
+    constraint FK_carts_items_items foreign key (item_id) references items (id) on delete cascade
+);
+
+create table if not exists users
+(
+    id           bigserial      not null,
+    name         varchar(45) not null,
+    email        varchar(45) not null unique,
+    password     varchar(65) not null,
+    phone_number varchar(45) not null unique,
+    cart_id      bigserial      not null unique,
+    created_at   timestamp   not null,
+    primary key (id),
+    constraint FK_users_carts foreign key (cart_id) references carts (id)
+);
+
+create table if not exists users_addresses
+(
+    user_id    bigserial not null,
+    address_id bigserial not null,
+    constraint FK_users_addresses_addresses foreign key (address_id) references addresses (id) on delete cascade,
+    constraint FK_users_addresses_users foreign key (user_id) references users (id) on delete cascade
+);
+
+create table if not exists orders
+(
+    id            bigserial      not null,
+    user_id       bigserial      not null,
+    address_id    bigserial      not null,
+    restaurant_id bigserial      not null,
+    cart_id       bigserial      not null,
+    status        varchar(45) not null,
+    courier_id    bigserial,
+    created_at    timestamp   not null,
+    delivered_at  timestamp,
+    primary key (id),
+    constraint FK_orders_users foreign key (user_id) references users (id) on delete cascade,
+    constraint FK_orders_addresses foreign key (address_id) references addresses (id),
+    constraint FK_orders_restaurants foreign key (restaurant_id) references restaurants (id) on delete cascade,
+    constraint FK_orders_carts foreign key (cart_id) references carts (id),
+    constraint FK_orders_couriers foreign key (courier_id) references couriers (id)
+);
+
+create table if not exists users_orders
+(
+    user_id  bigserial not null,
+    order_id bigserial not null,
+    constraint FK_users_orders_orders foreign key (order_id) references orders (id) on delete cascade,
+    constraint FK_users_orders_users foreign key (user_id) references users (id) on delete cascade
+);
